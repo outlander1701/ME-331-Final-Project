4213eead307cfb044bb6ecac6ebd9c3ff80b8505
bennet <bennet.outland@protonmail.com>
2023-04-29 11:55:14 -0600

    AAAAAAAGHH why is it due so soongit add .!

--- src/wt_int.jl
+++ src/wt_int.jl
@@ -1,7 +1,7 @@
 using CSV
 using DataFrames
 
-wt_data = CSV.File("./csv/wind_tunnel_data.csv")
+wt_data = CSV.File("./csv/wind_tunnel_test.csv")
 
 length_dict = Dict([["1", 0], ["2", 0.175], ["3", 0.35], ["4", 0.7], ["5", 1.05], ["6", 1.4], ["7", 1.75], ["8", 2.1], ["9", 2.45], ["10", 2.8], ["11", 0.175], ["12", 0.35], ["13", 0.7], ["14", 1.05], ["15", 1.4], ["16", 1.75], ["17", 2.1], ["18", 2.45]])
 
@@ -16,10 +16,8 @@ for i ∈ 2:10
 end
 
 # Params
-velocity = 60
+velocity = 40
 α = 0
-ρ = (23.77 * 10^(-4)) * (12^3) # https://www.engineeringtoolbox.com/standard-atmosphere-d_604.html
-A = 3.5 # in^2
 
 function trap_int(x, y)
     Δx = (x[end]-x[1])/length(x)
@@ -39,7 +37,8 @@ end
 function find_index(wt_data)
 
     for i ∈ eachindex(wt_data["Velocity [MPH]"])
-        if wt_data["Velocity [MPH]"][i] == velocity
+        if (wt_data["Velocity [MPH]"][i] == velocity) && (wt_data["Angle of Attack"][i] == α)
+            println(wt_data["Velocity [MPH]"][i], " ", wt_data["Angle of Attack"][i])
             return i
         end
     end
@@ -53,13 +52,15 @@ vel_index = find_index(wt_data)
 bottom_indices = (1 + vel_index):(9 + vel_index)
 
 lengths = []
+
 for key in bottom
     push!(lengths, length_dict["$key"])
 end
 
+
 bottom_extrap = wt_data["Pressure AVG [psi]"][bottom_indices[end]] * (3.5 - length_dict["18"]) * 1
 
-bottom_force = (trap_int(lengths, wt_data["Pressure AVG [psi]"][bottom_indices]) * 1) + bottom_extrap
+bottom_force = (trap_int(lengths, wt_data["Pressure AVG [psi]"][bottom_indices]) * 1) #+ bottom_extrap
 
 # Integrating the top
 
@@ -75,16 +76,27 @@ m_top = ((wt_data["Pressure AVG [psi]"][bottom_indices[9]] - wt_data["Pressure A
 
 top_extrap = wt_data["Pressure AVG [psi]"][bottom_indices[9]] * (3.5 - L_10) + 0.5 * m_top *(3.5 - L_10) * 1
 
-top_force = trap_int(lengths, wt_data["Pressure AVG [psi]"][top_indices]) * 1 + top_extrap
+top_force = trap_int(lengths, wt_data["Pressure AVG [psi]"][top_indices]) * 1 #+ top_extrap
 
 # Calculate C_L
-ΣF = top_force - bottom_force
-v = velocity * (5280*12)/(60*60) # in/s
+ΣF_freedom = bottom_force - top_force
+ΣF_frenchy = ΣF_freedom * 4.448222 # N
+v = velocity * 0.44704 # m/slugs
+A = 0.0889 # m^2
+ρ = 1.225 # kg/m^3
 
-C_L = (2 * ΣF) / (A * ρ * v^2)
+"""
+v = velocity * (5280*12)/(60*60) # in/s
+ρ = 0.00237691/(12^4) # slugs / in^3    #(23.77 * 10^(-4)) * (12^3) # https://www.engineeringtoolbox.com/standard-atmosphere-d_604.html
+A = 3.5 # in^2
+"""
+
+C_L = (2 * ΣF_frenchy) / (A * ρ * v^2)
 
 println("\n+===================================+")
-println("ΣF [lbf]: ", ΣF)
+println("Top [lbf]: ", top_force)
+println("Bottom [lbf]: ", bottom_force)
+println("ΣF [lbf]: ", ΣF_freedom)
 println("Velocity [MPH]: ", velocity)
 println("α [°]: ", α)
 println("C_L: ", C_L)
