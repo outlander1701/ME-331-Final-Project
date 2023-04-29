b509626d66b82c06a2c6c778f697be38a95c9e56
Joshua <jhoff2021@gmail.com>
2023-04-29 11:58:46 -0600

    Co-authored-by: Bennet Outland <outlander1701@users.noreply.github.com>

---
+++ src/CFD.jl
@@ -0,0 +1,23 @@
+using CSV, Plots
+
+include("./utilities.jl")
+
+Data = CSV.File("./csv/CFDForceData.csv")
+
+F_d = Data["F_D"]
+F_l = Data["F_L"]
+
+V = Data["Velocity"]
+α = Data["AoA"]
+
+C_d = Coefficient(F_d, V)
+C_l = Coefficient(F_l, V)
+
+Cd_sorted = reshape(C_d, 4, 8)
+Cl_sorted = reshape(C_l, 4, 8)
+α_sorted = reshape(α, 4, 8)
+
+#plot_c_vs_α(α_sorted, Cl_sorted)
+
+plot_c_vs_α(α_sorted, Cd_sorted)
+print(Cl_sorted)
--- src/wt_int.jl
+++ src/wt_int.jl
@@ -18,17 +18,20 @@ end
 # Params
 velocity = 60
 α = 0
-ρ = (23.77 * 10^(-4)) * (12^3) # https://www.engineeringtoolbox.com/standard-atmosphere-d_604.html
-A = 3.5 # in^2
+ρ = 0.0765 #(lbm/ft^3) https://www.engineersedge.com/calculators/air-density.htm#:~:text=At%20sea%20level%20and%20at,ISA%20(International%20Standard%20Atmosphere
+A = 3.5/144 # ft^2
 
 function trap_int(x, y)
     Δx = (x[end]-x[1])/length(x)
 
     sum = Δx*0.5*y[1] + Δx*0.5*y[end]
+    #println("Init: ", sum)
 
     for i ∈ 2:(length(x)-1)
         sum = sum + Δx*y[i]
     end
+
+    #println("Fin: ", sum)
 
     return sum
 
@@ -36,60 +39,62 @@ end
 
 # Find the data
 
-function find_index(wt_data)
+function find_index(wt_data, velocity, α)
 
     for i ∈ eachindex(wt_data["Velocity [MPH]"])
         if wt_data["Velocity [MPH]"][i] == velocity
-            return i
+            if wt_data["Angle of Attack"][i] == α
+                return i
+            end
         end
     end
 
 end
 
-vel_index = find_index(wt_data)
+function find_Cl(wt_data, velocity, α, ρ, A)
+    vel_index = find_index(wt_data, velocity, α)
 
-# Integrating the bottom
+    # Integrating the bottom
 
-bottom_indices = (1 + vel_index):(9 + vel_index)
+    bottom_indices = (10 + vel_index):(17 + vel_index)
 
-lengths = []
-for key in bottom
+    lengths = []
+    for key in bottom
     push!(lengths, length_dict["$key"])
-end
+    end
 
-bottom_extrap = wt_data["Pressure AVG [psi]"][bottom_indices[end]] * (3.5 - length_dict["18"]) * 1
+    bottom_extrap = wt_data["Pressure AVG [psi]"][bottom_indices[end]] * (3.5 - length_dict["18"]) * 1
 
-bottom_force = (trap_int(lengths, wt_data["Pressure AVG [psi]"][bottom_indices]) * 1) + bottom_extrap
+    bottom_force = (trap_int(lengths, wt_data["Pressure AVG [psi]"][bottom_indices]) * 1) + bottom_extrap
 
-# Integrating the top
+    # Integrating the top
 
-top_indices = (10 + vel_index):(17 + vel_index)
+    top_indices = (1 + vel_index):(9 + vel_index)
 
-lengths = []
-for key in top
+    lengths = []
+    for key in top
     push!(lengths, length_dict["$key"])
+    end
+
+    L_10 = length_dict["10"]
+    m_top = ((wt_data["Pressure AVG [psi]"][top_indices[9]] - wt_data["Pressure AVG [psi]"][top_indices[8]]) / (L_10 - length_dict["9"]))
+
+    top_extrap = wt_data["Pressure AVG [psi]"][top_indices[9]] * (3.5 - L_10) + 0.5 * m_top *(3.5 - L_10) * 1
+
+    top_force = trap_int(lengths, wt_data["Pressure AVG [psi]"][top_indices]) * 1 + top_extrap
+
+    # Calculate C_L
+    ΣF = top_force - bottom_force
+    v = velocity * (5280)/(60*60) # ft/s
+
+    C_L = (2 * ΣF) / (A * ρ * v^2)
+
+    println("\n+===================================+")
+    println("ΣF [lbf]: ", ΣF)
+    println("Velocity [MPH]: ", velocity)
+    println("α [°]: ", α)
+    println("C_L: ", C_L)
 end
-
-L_10 = length_dict["10"]
-m_top = ((wt_data["Pressure AVG [psi]"][bottom_indices[9]] - wt_data["Pressure AVG [psi]"][bottom_indices[8]]) / (L_10 - length_dict["9"]))
-
-top_extrap = wt_data["Pressure AVG [psi]"][bottom_indices[9]] * (3.5 - L_10) + 0.5 * m_top *(3.5 - L_10) * 1
-
-top_force = trap_int(lengths, wt_data["Pressure AVG [psi]"][top_indices]) * 1 + top_extrap
-
-# Calculate C_L
-ΣF = top_force - bottom_force
-v = velocity * (5280*12)/(60*60) # in/s
-
-C_L = (2 * ΣF) / (A * ρ * v^2)
-
-println("\n+===================================+")
-println("ΣF [lbf]: ", ΣF)
-println("Velocity [MPH]: ", velocity)
-println("α [°]: ", α)
-println("C_L: ", C_L)
-
-
 
 
 
